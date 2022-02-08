d5b9f55cd323ebf5c83669547b85ae9c39551c04
rainyShine <fu0k0ffq11tq@gmail.com>
2022-02-08 23:44:38 +0900

    init

---
+++ src/main/java/common/Poyo.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.util.Collections;
+
+/**
+ * Do nothing stub Class
+ */
+public class Poyo {
+	public static void main(String... args) {
+		System.out.println(Collections.emptyList().toString());
+	}
+}
